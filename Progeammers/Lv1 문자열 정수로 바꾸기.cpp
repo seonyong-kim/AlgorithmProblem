2d74288d997fdc0cce131570068ac2b736995bed
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2024-12-29 15:28:59 +0900

    Add files via upload

---
+++ Progeammers/Lv1 문자열 정수로 바꾸기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    if (s[0] == '-') {
+        for (int i = 1; i < s.size(); i++) {
+            answer += -((s[i] - 48) * pow(10, s.size() - i - 1));
+        }
+    }
+    else if (s[0] == '+') {
+        for (int i = 1; i < s.size(); i++) {
+            answer += (s[i] - 48) * pow(10, s.size() - i - 1);
+        }
+    }
+    else {
+        for (int i = 0; i < s.size(); i++) {
+            answer += (s[i] - 48) * pow(10, s.size() - 1 - i);
+        }
+    }
+    return answer;
+}
