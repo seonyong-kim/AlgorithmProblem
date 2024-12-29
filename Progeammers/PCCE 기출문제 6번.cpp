4cb5c4a5b1c21d65ed7c9308d68fc3a383ab5a21
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2024-12-29 12:40:38 +0900

    Update PCCE 기출문제 6번.cpp

--- Progeammers/PCCE 기출문제 6번.cpp
+++ Progeammers/PCCE 기출문제 6번.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int solution(int storage, int usage, vector<int> change) {
     int total_usage = 0;
     for (int i = 0; i < change.size(); i++) {
-        usage += usage * change[i] / 100;
+        usage += usage * change[i] / 100; //이부분 변경
         total_usage += usage;
         if (total_usage > storage) {
             return i;
