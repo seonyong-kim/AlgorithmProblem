61095d38a54a358b4a7004509273a97017bfe5ba
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2024-12-29 16:24:28 +0900

    Update PCCE 기출문제 3번 나이계산.cpp

--- Progeammers/Lv 0/PCCE 기출문제 3번 나이계산.cpp
+++ Progeammers/Lv 0/PCCE 기출문제 3번 나이계산.cpp
@@ -7,19 +7,12 @@ int main(void) {
     string age_type;
     cin >> year >> age_type;
 
-    if (age_type ==
-        "Korea"
-        ) {
-        answer =
-            2030 - year + 1
-            ;
+    if (age_type =="Korea") {
+        answer =2030 - year + 1;
     }
     else if (age_type == "Year") {
-
-        answer = 2030 - year
-            ;
+        answer = 2030 - year;
     }
-
     cout << answer << endl;
     return 0;
 }
