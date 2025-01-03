4761142ff83eda58e92aceffc74a2ec1046624c1
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2025-01-03 12:42:44 +0900

    Add files via upload

---
+++ Progeammers/Lv 1/서울에서 김서장 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    int index = 0;
+    for (int i = 0; i < seoul.size(); i++) {
+        if (seoul[i] == "Kim") {
+            index = i;
+            break;
+        }
+    }
+    return "김서방은 " + to_string(index) + "에 있다";
+}
