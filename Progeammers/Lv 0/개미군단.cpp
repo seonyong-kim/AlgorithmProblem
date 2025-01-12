489684c4f079da7a8473c627570231a274852f0c
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2025-01-13 02:06:16 +0900

    Add files via upload

---
+++ Progeammers/Lv 0/개미군단.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    for (int i = 5; i >= 1; i -= 2) {
+        answer += hp / i;
+        hp -= i * (hp / i);
+    }
+    return answer;
+}
