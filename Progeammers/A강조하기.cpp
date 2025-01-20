24254c4d1a762031a4e11f0a942774e2c669a84c
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2025-01-20 13:14:43 +0900

    Add files via upload

---
+++ Progeammers/A강조하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for (int i = 0; i < myString.size(); i++) {
+        if (myString[i] == 'a')
+            myString[i] -= 32;
+        else if (myString[i] > 'A' && myString[i] <= 'Z')
+            myString[i] += 32;
+    }
+    return myString;
+}
---
+++ Progeammers/특정한 문자를 대문자로 바꾸기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string alp) {
+    for (int i = 0; i < my_string.length(); i++) {
+        if (my_string[i] == alp[0])
+            my_string[i] -= 32;
+    }
+    return my_string;
+}
