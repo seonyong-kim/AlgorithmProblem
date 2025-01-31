81fb13088203cf85f98774fd9a9f85244d1c8562
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2025-01-31 14:18:55 +0900

    Add files via upload

--- backjune/1003 피보나치 수열.cpp
+++ backjune/1003 피보나치 수열.cpp
@@ -3,39 +3,24 @@
 
 using namespace std;
 
-vector<pair<long long, long long>> fibonacci(int n);
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int T;
-    cin >> T;
-    while (T--) {
-        int N;
-        cin >> N;
-        vector<pair<long long, long long>> count = fibonacci(N);
-        cout << count[N].first << " " << count[N].second << "\n";
+    int x;
+    cin >> x;
+
+    vector<int> dp(x + 1, 0);
+    for (int i = 2; i <= x; i++) {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 3 == 0) {
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+        }
+        if (i % 2 == 0) {
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
     }
 
-}
-
-vector<pair<long long, long long>> fibonacci(int n) {
-    vector<pair<long long, long long>> dp(n+1);
-    if (n == 0) {
-        dp[0].first++;
-        return dp;
-    }
-    else if (n == 1) {
-        dp[1].second++;
-        return dp;
-    }
-    dp[0].first++;
-    dp[1].second++;
-    for (int i = 2; i <= n; i++) {
-        dp[i].first = dp[i - 1].first + dp[i - 2].first;
-        dp[i].second = dp[i - 1].second + dp[i - 2].second;
-    }
-    return dp;
+    cout << dp[x];
 }
