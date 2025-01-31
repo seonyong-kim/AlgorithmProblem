bcaea73cb1319283ca5e8320f852755394fbb796
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2025-01-31 14:21:10 +0900

    Add files via upload

---
+++ backjune/1463 1로 만들기.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int x;
+    cin >> x;
+
+    vector<int> dp(x + 1, 0);
+    for (int i = 2; i <= x; i++) {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 3 == 0) {
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+        }
+        if (i % 2 == 0) {
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
+    }
+
+    cout << dp[x];
+}
