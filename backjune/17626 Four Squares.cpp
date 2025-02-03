2a99c34f96bab41f0428e641fe847c84c8f2b9f4
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2025-02-03 20:59:01 +0900

    Add files via upload

---
+++ backjune/17626 Four Squares.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1, INT_MAX);
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j * j <= i; j++) {
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+    cout << dp[n];
+}
