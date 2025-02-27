b5b7c8c5387b62645bc995b421c1f4c9d32bdad2
seonyong-kim <141008683+seonyong-kim@users.noreply.github.com>
2025-02-27 10:31:30 +0900

    Add files via upload

---
+++ backjune/15663 N과 M (9).cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+void bactstrace();
+vector<int> answer;
+vector<bool> visited(8, false);
+map<int, int> numCount;
+int N, M;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+    int a, b;
+    for (int i = 0; i < N; i++) {
+        cin >> a;
+        numCount[a]++;
+    }
+
+    bactstrace();
+}
+
+void bactstrace() {
+    if (answer.size() == M) {
+        for (int i : answer) {
+            cout << i << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (auto& pair : numCount) {
+        int num = pair.first;
+        int& count = pair.second;
+
+        if (count > 0) {
+            count--;
+            answer.emplace_back(num);
+            bactstrace();
+            answer.pop_back();
+            count++;
+        }
+    }
+}
